037305ee74ff6e01b791c564d1ac14412fe6ee27
Donald Dominic <gotodonald@gmail.com>
2022-12-16 13:21:45 +0100

    XML encoding

--- encodings/main.go
+++ encodings/main.go
@@ -5,7 +5,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"encoding/json"
-	// "encoding/xml"
+	"encoding/xml"
 	// "encoding/csv"
 	// "os"
 )
@@ -16,6 +16,7 @@ func main() {
 	encodeBase64(message)
 	hexEncoding(message)
 	jsonEncoding()
+	xmlEncoding()
 }
 
 func encodeBase64(message string) {
@@ -70,4 +71,50 @@ func jsonEncoding() {
 	fmt.Printf("Id: %d\n",newEmp.Id)
 	fmt.Printf("Name: %s\n",newEmp.Name)
 	fmt.Printf("Email: %s\n",newEmp.Email)
+}
+
+func xmlEncoding() {
+	fmt.Println("------------ XML Encoding example ----------------")
+	type EmployeeCountry struct {
+		CountryCode string `xml: "code, attr"` // XML attribute code
+		CountryName string `xml: ",chardata"` // XML value
+	}
+
+	type Employee struct {
+		XMLName xml.Name `xml:"employee"`
+		Id string `xml:"id"`
+		Name string `xml:"name"`
+		Email string `xml:"email"`
+		Country EmployeeCountry `xml:"country"`
+	}
+
+	// struct to xml
+	fmt.Println(">>>>struct to xml….")
+	emp := &Employee{
+		Id:"12345",
+		Name:"Michael",
+		Email:"michael@email.com",
+		Country: EmployeeCountry{
+			CountryCode:"DE",
+			CountryName: "DE",
+		},
+	}
+
+	encoded, err := xml.Marshal(emp)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(encoded))
+
+	// xml string to struct
+	fmt.Println(">>>>xml to struct….")
+	var newEmp Employee
+	// str := `<employee><id>555</id><name>Lee</name><email>lee@email.com</email><country code="CN">China</country></employee>`
+	xml.Unmarshal([]byte(encoded), &newEmp)
+	fmt.Printf("Id: %s\n", newEmp.Id)
+	fmt.Printf("Name: %s\n", newEmp.Name)
+	fmt.Printf("Email: %s\n", newEmp.Email)
+	fmt.Printf("CountryCode: %s\n", newEmp.Country.CountryCode)
+	fmt.Printf("CountryName: %s\n", newEmp.Country.CountryName)
 }
