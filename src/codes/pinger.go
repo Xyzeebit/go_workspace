a4f9ea6a55f061fdd37965b92214425c7874296f
Donald Dominic <gotodonald@gmail.com>
2023-01-04 09:11:19 +0100

    gochannels

--- src/codes/pinger.go
+++ src/codes/pinger.go
@@ -5,15 +5,21 @@ import (
 	"time"
 )
 
-func pinger(c chan string) {
+func pinger(c chan<- string) {
 	for i := 0; ; i++ {
 		c <- "ping"
 	}
 }
 
+func ponger(c chan<- string) {
+	for i := 0; ; i++ {
+		c <- "pong"
+	}
+}
+
 func printer(c chan string) {
 	for {
-		msg := <-c
+		msg := <- c
 		fmt.Println(msg);
 		time.Sleep(time.Second * 1);
 	}
@@ -23,6 +29,7 @@ func main() {
 	var c chan string = make(chan string);
 
 	go pinger(c);
+	go ponger(c);
 	go printer(c);
 
 	var input string;
