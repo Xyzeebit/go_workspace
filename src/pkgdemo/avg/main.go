2e3a9225b5d90afab9d2f435bd427bdc19963671
Donald Dominic <gotodonald@gmail.com>
2023-01-04 10:37:04 +0100

    go packages demo

--- src/pkgdemo/avg/main.go
+++ src/pkgdemo/avg/main.go
@@ -6,8 +6,11 @@ import (
 )
 
 func main() {
-	xs := []float64{1, 2, 3, 4}
+	xs := []float64{1, 2, 3, 4, 0.3}
 	avg := math.Average(xs)
 	fmt.Println(avg);
-
+	min := math.Min(xs);
+	fmt.Println(min);
+	max := math.Max(xs);
+	fmt.Println(max);
 }
--- src/pkgdemo/math/math.go
+++ src/pkgdemo/math/math.go
@@ -1,9 +1,50 @@
 package math
 
+// returns the average of a slice of float64 numbers
+//
+//     x := []float64{1,2,3}
+//
+//     y := math.Average(x)
 func Average(xs []float64) float64 {
 	total := float64(0);
 	for _, i := range xs {
 		total += i;
 	}
 	return total / float64(len(xs));
+}
+
+// Finds the minimum number in a slice of numbers
+//
+//     x := []float64{1, 3, 2}
+//
+//     y := math.Min(x)
+//
+//     fmt.Println(y) // 1
+//
+func Min(numbers []float64) float64 {
+	var min float64 = numbers[0];
+	for _, i := range numbers {
+		if i < min {
+			min = i;
+		}
+	}
+	return min
+}
+
+// Finds the maximum number in a slice of numbers
+//
+//     x := []float64{1, 3, 2}
+//
+//     y := math.Max(x)
+//
+//     fmt.Println(y) // 3
+//
+func Max(numbers []float64) float64 {
+	var max float64 = numbers[0];
+	for _, i := range numbers {
+		if max < i {
+			max = i;
+		}
+	}
+	return max;
 }
