075f44f6ef3d7a9eff0a17c3d88a04882bd342df
Donald Dominic <gotodonald@gmail.com>
2023-01-05 21:50:23 +0100

    implementing sorting

--- src/collections/sorting.go
+++ src/collections/sorting.go
@@ -11,16 +11,26 @@ type Person struct {
 }
 
 type ByName []Person
+type ByAge []Person
 
 func (this ByName) Len() int {
+	return len(this)
+}
+func (this ByAge) Len() int {
 	return len(this)
 }
 
 func (this ByName) Less(i, j int) bool {
 	return this[i].Name < this[j].Name
 }
+func (this ByAge) Less(i, j int) bool {
+	return this[i].Age < this[j].Age
+}
 
 func (this ByName) Swap(i, j int) {
+	this[i], this[j] = this[j], this[i]
+}
+func (this ByAge) Swap(i, j int) {
 	this[i], this[j] = this[j], this[i]
 }
 
@@ -29,7 +39,10 @@ func main() {
 	kids := []Person{
 		{"Jill", 9},
 		{"Jack", 10},
+		{"john", 8},
 	}
 	sort.Sort(ByName(kids))
+	fmt.Println(kids)
+	sort.Sort(ByAge(kids))
 	fmt.Println(kids)
 }
