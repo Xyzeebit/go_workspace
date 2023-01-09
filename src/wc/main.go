26238d0165daef7078dc9117fb0a8f439075c31d
Donald Dominic <gotodonald@gmail.com>
2023-01-09 23:01:41 +0100

    added wc counter

--- src/wc/main.go
+++ src/wc/main.go
@@ -1,10 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
 	fmt.Println(os.Stdin);
+}
+
+// A counter function the counts the words read by the scanner
+// Returns the number of words counted
+func count(r io.Reader) int {
+	// A Scanner is use to read text from a Reader (such as files)
+	scanner := bufio.NewScanner(r);
+
+	// Define the Scanner split type to words (default is split by line)
+	scanner.Split(bufio.ScanWords);
+
+	// Define a counter
+	wc := 0;
+
+	// For every word scanned, increment the counter
+	for scanner.Scan() {
+		wc++;
+	}
+
+	// Return the total
+	return wc;
 }
