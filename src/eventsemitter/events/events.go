e881a04c70e432e46531b8873c5d5fcf07813d7a
Donald Dominic <gotodonald@gmail.com>
2023-01-09 17:01:59 +0100

    moved event emitter to new package

--- src/eventsemitter/events/events.go
+++ src/eventsemitter/events/events.go
@@ -1,14 +1,36 @@
 package events
 
-type Fn[F any] func(F);
+import "eventsemitter/events"
 
-type Emitter struct {
-	events map[string][]interface{}
+type Fn[T any] func(T)
+
+type Emitter[T any] struct {
+	events map[string][]Fn[T]
 }
 
+func (e *Emitter[T]) emit(event string, data T) (evt Emitter[T]) {
+	listeners, ok := e.events[event]
+	if !ok {
+		return
+	}
+	for _, listener := range listeners {
+		listener(data)
+	}
+	return *e;
+}
 
+func (e *Emitter[T]) on(event string, fn Fn[T]) (evt Emitter[T]) {
+	_, ok := e.events[event];
+	if !ok {
+		v := []Fn[T]{ fn };
+		e.events[event] = v;
+		return *e;
+	}
+	e.events[event] = append(e.events[event], fn);
+	return *e;
+}
 
-func EventsEmitter() Emitter {
-	var events Emitter;
+func EventsEmitter[T any]() Emitter[T] {
+	var events Emitter[T]
 	return events
 }
