aec8876ffcd8a0da1f217d072fb9452fc62fd1d0
Donald Dominic <gotodonald@gmail.com>
2023-01-09 23:24:18 +0100

    Added lines count flag to app

--- src/wc/main.go
+++ src/wc/main.go
@@ -2,24 +2,33 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
-	fmt.Println(count(os.Stdin));
+	// Defining a boolean flag -l to count lines instead of words
+	lines := flag.Bool("l", false, "Count lines");
+	// Parsing the flags provided by the user
+	flag.Parse();
+
+	fmt.Println(count(os.Stdin, *lines));
 }
 
 // A counter function that counts the words read by the scanner
 //
 // Returns the number of words counted
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool) int {
 	// A Scanner is use to read text from a Reader (such as files)
 	scanner := bufio.NewScanner(r);
 
-	// Define the Scanner split type to words (default is split by line)
-	scanner.Split(bufio.ScanWords);
+	// If the count lines flag is not set, we want to count words so we define
+	// the Scanner split type to words (default is split by line)
+	if !countLines {
+		scanner.Split(bufio.ScanWords);
+	}
 
 	// Define a counter
 	wc := 0;
