c7eb28a4d36342e3ed429a8640c18e9f632d7ae2
Donald Dominic <gotodonald@gmail.com>
2023-01-11 10:04:07 +0100

    using event emitter

--- src/eventsemitter/evt/main.go
+++ src/eventsemitter/evt/main.go
@@ -13,8 +13,23 @@ func main() {
 		fmt.Println(n)
 	});
 	emitter.Emit("hello", 20);
+
+	emitter.On("count", counter);
+	emitter.On("count", countdown);
+	startCounting(emitter);
+}
+
+func counter(n int) {
+	fmt.Print("counter: ", n, " ");
+}
+
+func countdown(n int) {
+	fmt.Println("count down:", n - 1);
+}
+
+func startCounting(emitter *events.Emitter[int]) {
 	for i := 0; i < 10; i++ {
 		n := rand.Intn(10);
-		emitter.Emit("hello", n);
+		emitter.Emit("count", n);
 	}
 }
